b222cc1830113d1e1ec3d5eb58e94fd4a35abd6a
yerkebulan.omar <yerkebulan.omar@gmail.com>
2023-01-23 18:02:07 +0600

    hw nearly done - need to review after watching additional videos

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-document.getElementById("hider").onclick = function () {
-  document.getElementById("text").hidden = true;
-};
+// document.getElementById("hider").onclick = function () {
+//   document.getElementById("text").hidden = true;
+// };
+
+button = document.querySelector("#hider");
+
+button.addEventListener("click", () => alert("1"));
+
+button.removeEventListener("click", () => alert("1")); // doesn't work
+
+button.onclick = () => alert(2);
